3e60211f0a0d0c30e00f11a1b65669c09dfb8c18
Mateusz Kondej <m@kondej.net>
2023-03-19 20:28:48 +0000

    Add option to load LDIF files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::convert::AsRef;
+use std::path::Path;
 use std::{
     io::{BufRead, BufReader},
     net::{TcpStream, ToSocketAddrs},
@@ -5,67 +7,110 @@ use std::{
     thread::sleep,
     time::Duration,
 };
+use url::Url;
 
 use tempfile::{tempdir, TempDir};
 use timeout_readwrite::TimeoutReader;
 
-const SLAPD_INIT_LDIF: &str = include_str!("slapd.init.ldif");
+const INIT_LDIF: &str = include_str!("init.ldif");
+const POSSIBLE_SCHEMA_DIR: &[&str] = &[
+    "/etc/ldap/schema",
+    "/usr/local/etc/openldap/schema",
+    "/etc/openldap/schema/",
+];
 
 pub struct LdapServerBuilder {
+    dir: TempDir,
     base_dn: String,
-    root_dn: Option<String>,
-    root_pw: Option<String>,
+    root_dn: String,
+    root_pw: String,
 }
 
 impl LdapServerBuilder {
-    pub fn new(base_dn: &str) -> Self {
+    pub fn init(base_dn: &str) -> Self {
+        LdapServerBuilder::init_with_config(base_dn, INIT_LDIF)
+    }
+
+    pub fn init_with_config(base_dn: &str, init_config: &str) -> Self {
+        let root_dn = format!("cn=admin,{base_dn}");
+        let root_pw = "secret".to_string();
+
+        let schema_dir = find_slapd_schema_dir()
+            .expect("no slapd schema directory found. Is openldap server installed?");
+        let schema_dir = Url::from_file_path(schema_dir).unwrap();
+
+        let init_ldif = init_config
+            .replace("@SCHEMADIR@", schema_dir.as_ref())
+            .replace("@BASEDN@", base_dn)
+            .replace("@ROOTDN@", &root_dn)
+            .replace("@ROOTPW@", &root_pw);
+
+        let builder = LdapServerBuilder::init_empty(base_dn, root_dn, root_pw);
+        builder.add_ldif(&init_ldif, 0)
+    }
+
+    pub fn init_empty(
+        base_dn: impl Into<String>,
+        root_dn: impl Into<String>,
+        root_pw: impl Into<String>,
+    ) -> Self {
+        let dir = tempdir().unwrap();
+        let base_dn = base_dn.into();
+        let root_dn = root_dn.into();
+        let root_pw = root_pw.into();
+
         Self {
-            base_dn: base_dn.to_string(),
-            root_dn: None,
-            root_pw: None,
+            dir,
+            base_dn,
+            root_dn,
+            root_pw,
         }
+    }
+
+    pub fn add_ldif(self, ldif_text: &str, database_number: u8) -> Self {
+        let tmp_ldif = self.dir.path().join("tmp.ldif");
+        std::fs::write(&tmp_ldif, ldif_text).unwrap();
+        self.add_ldif_file(tmp_ldif, database_number)
+    }
+
+    pub fn add_ldif_file<P: AsRef<Path>>(self, file: P, database_number: u8) -> Self {
+        let file = file.as_ref();
+
+        let db_number = database_number.to_string();
+        // load slapd configuration
+        let output = Command::new("slapadd")
+            .args(["-F", ".", "-n"])
+            .arg(db_number)
+            .arg("-l")
+            .arg(file)
+            .current_dir(&self.dir)
+            .output()
+            .expect("failed to execute slapadd");
+
+        if !output.status.success() {
+            panic!(
+                "slapadd command exited with error {}, stdout: {}, stderr: {} on file {}",
+                output.status,
+                String::from_utf8_lossy(&output.stdout),
+                String::from_utf8_lossy(&output.stderr),
+                file.display()
+            );
+        }
+
+        self
     }
 
     // Start LDAP server
     pub fn run(self) -> LdapServerConn {
         let host = "localhost".to_string();
         let port = portpicker::pick_unused_port().expect("no free tcp port to bind");
-        let dir = tempdir().unwrap();
         let url = format!("ldap://{host}:{port}");
-
-        let base_dn = self.base_dn;
-        let root_dn = self
-            .root_dn
-            .unwrap_or_else(|| format!("cn=admin,{base_dn}"));
-        let root_pw = self.root_pw.unwrap_or_else(|| "secret".to_string());
-
-        let slpad_init = SLAPD_INIT_LDIF
-            .replace("@BASEDN@", &base_dn)
-            .replace("@ROOTDN@", &root_dn)
-            .replace("@ROOTPW@", &root_pw);
-
-        let slapd_init_file = dir.path().join("slapd.init.ldif");
-        std::fs::write(&slapd_init_file, &slpad_init).unwrap();
-
-        // load slapd configuration
-        let output = Command::new("slapadd")
-            .args(["-F", ".", "-n", "0", "-l", "slapd.init.ldif"])
-            .current_dir(&dir)
-            .output()
-            .expect("failed to execute slapadd");
-
-        if !output.status.success() {
-            panic!("slapadd command exited with error {}, stdout: {}, stderr: {}, slapd.init.ldif: {slpad_init}",
-            output.status,
-            String::from_utf8_lossy(&output.stdout),
-            String::from_utf8_lossy(&output.stderr));
-        }
 
         // lauch slapd server
         let mut server = Command::new("slapd")
             .args(["-F", ".", "-d", "2048", "-h", &url])
             .stderr(Stdio::piped())
-            .current_dir(&dir)
+            .current_dir(&self.dir)
             .spawn()
             .unwrap();
 
@@ -91,7 +136,7 @@ impl LdapServerBuilder {
 
         // test if tcp port is open
         if !is_tcp_port_open(&host, port) {
-            println!("tcp port {port} is not open yet");
+            println!("tcp port {port} is not open yet, waiting...");
             sleep(Duration::from_secs(1));
         }
 
@@ -106,13 +151,22 @@ impl LdapServerBuilder {
             url,
             host,
             port,
-            dir,
-            base_dn,
-            root_dn,
-            root_pw,
+            dir: self.dir,
+            base_dn: self.base_dn,
+            root_dn: self.root_dn,
+            root_pw: self.root_pw,
             server,
         }
     }
+}
+fn find_slapd_schema_dir() -> Option<&'static Path> {
+    for dir in POSSIBLE_SCHEMA_DIR {
+        let dir: &Path = dir.as_ref();
+        if dir.is_dir() {
+            return Some(dir);
+        }
+    }
+    None
 }
 
 fn is_tcp_port_open(host: &str, port: u16) -> bool {
@@ -157,6 +211,52 @@ impl LdapServerConn {
     pub fn root_pw(&self) -> &str {
         &self.root_pw
     }
+
+    pub fn add_ldif(&self, ldif_text: &str) -> &Self {
+        let tmp_ldif = self.dir.path().join("tmp.ldif");
+        std::fs::write(&tmp_ldif, ldif_text).unwrap();
+        self.add_ldif_file(tmp_ldif)
+    }
+
+    pub fn add_ldif_config(&self, ldif_text: &str) -> &Self {
+        let tmp_ldif = self.dir.path().join("tmp.ldif");
+        std::fs::write(&tmp_ldif, ldif_text).unwrap();
+        self.add_ldif_file_binddn(tmp_ldif, "cn=config", "secret");
+        self
+    }
+
+    pub fn add_ldif_file<P: AsRef<Path>>(&self, file: P) -> &Self {
+        self.add_ldif_file_binddn(file, self.root_dn(), self.root_pw());
+        self
+    }
+
+    pub fn add_ldif_file_binddn<P: AsRef<Path>>(
+        &self,
+        file: P,
+        binddn: &str,
+        password: &str,
+    ) -> &Self {
+        let file = file.as_ref();
+
+        let output = Command::new("ldapadd")
+            .args(["-x", "-D", binddn, "-w", password, "-H", self.url(), "-f"])
+            .arg(file)
+            .current_dir(&self.dir)
+            .output()
+            .expect("failed to execute ldapadd");
+
+        if !output.status.success() {
+            panic!(
+                "ldapadd command exited with error {}, stdout: {}, stderr: {} on file {}",
+                output.status,
+                String::from_utf8_lossy(&output.stdout),
+                String::from_utf8_lossy(&output.stderr),
+                file.display()
+            );
+        }
+
+        self
+    }
 }
 
 impl Drop for LdapServerConn {
@@ -176,11 +276,44 @@ impl Drop for LdapServerConn {
 #[cfg(test)]
 mod tests {
 
-    use super::LdapServerBuilder;
+    use super::*;
 
     #[test]
     fn run_slapd() {
-        let server = LdapServerBuilder::new("dc=kondej,dc=net").run();
+        let server = LdapServerBuilder::init("dc=planetexpress,dc=com")
+            .add_ldif(
+                "dn: dc=planetexpress,dc=com
+objectclass: dcObject
+objectclass: organization
+o: Planet Express
+dc: planetexpress
+
+dn: ou=people,dc=planetexpress,dc=com
+objectClass: top
+objectClass: organizationalUnit
+description: Planet Express crew
+ou: people",
+                1,
+            )
+            .run();
+
+        server.add_ldif(
+            "dn: cn=Amy Wong+sn=Kroker,ou=people,dc=planetexpress,dc=com
+objectClass: top
+objectClass: person
+objectClass: organizationalPerson
+objectClass: inetOrgPerson
+cn: Amy Wong
+sn: Kroker
+description: Human
+givenName: Amy
+mail: amy@planetexpress.com
+ou: Intern
+uid: amy
+userPassword:: e1NTSEF9d0p2OXMyWjltMGJTMFIxV1k3QjdCRWZEVVZPQzg2Y3BWL3VDMHc9PQ=
+ =",
+        );
+
         println!("server: {server:?}");
     }
 }
--- tests/ldap-rs-crate.rs
+++ tests/ldap-rs-crate.rs
@@ -1,9 +1,10 @@
-use ldap_rs::LdapClient;
+use futures_util::TryStreamExt;
+use ldap_rs::{LdapClient, SearchRequest, SearchRequestScope};
 use ldap_test_server::LdapServerBuilder;
 
 #[tokio::test]
 async fn test_bind() {
-    let server = LdapServerBuilder::new("dc=kondej,dc=net").run();
+    let server = LdapServerBuilder::init("dc=kondej,dc=net").run();
 
     let mut client = LdapClient::builder(server.host())
         .port(server.port())
@@ -14,11 +15,110 @@ async fn test_bind() {
         .simple_bind(server.root_dn(), server.root_pw())
         .await
         .unwrap();
-    println!("Bind succeeded!");
 
     let authz = client.whoami().await.unwrap();
-    println!("Authz: {authz:?}");
+    assert_eq!(authz.as_deref(), Some("dn:cn=admin,dc=kondej,dc=net"));
 
     client.unbind().await.unwrap();
-    println!("Unbind succeeded!");
+}
+
+#[tokio::test]
+async fn test_query() {
+    let server = LdapServerBuilder::init("dc=planetexpress,dc=com").run();
+
+    server
+        .add_ldif(
+            "dn: dc=planetexpress,dc=com
+objectclass: dcObject
+objectclass: organization
+o: Planet Express
+dc: planetexpress
+
+dn: ou=people,dc=planetexpress,dc=com
+objectClass: top
+objectClass: organizationalUnit
+description: Planet Express crew
+ou: people",
+        )
+        .add_ldif(
+            "dn: cn=Philip J. Fry,ou=people,dc=planetexpress,dc=com
+objectClass: inetOrgPerson
+objectClass: organizationalPerson
+objectClass: person
+objectClass: top
+cn: Philip J. Fry
+sn: Fry
+description: Human
+displayName: Fry
+employeeType: Delivery boy
+givenName: Philip
+mail: fry@planetexpress.com
+ou: Delivering Crew
+uid: fry
+userPassword:: e3NzaGF9d0wvVG0wSHNaeU90K29jbXlrU290UkpURnczd0ZKOWRlaEU4eFE9PQ==
+",
+        );
+
+    let mut client = LdapClient::builder(server.host())
+        .port(server.port())
+        .connect()
+        .await
+        .unwrap();
+    client
+        .simple_bind(server.root_dn(), server.root_pw())
+        .await
+        .unwrap();
+
+    let result = client
+        .search(
+            SearchRequest::builder()
+                .base_dn(server.base_dn())
+                .scope(SearchRequestScope::WholeSubtree)
+                .filter("(objectClass=inetOrgPerson)")
+                .build()
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    let items = result.try_collect::<Vec<_>>().await.unwrap();
+    assert_eq!(items.len(), 1);
+    assert_eq!(
+        items[0].dn,
+        "cn=Philip J. Fry,ou=people,dc=planetexpress,dc=com"
+    );
+
+    server.add_ldif(
+        "dn: cn=Turanga Leela,ou=people,dc=planetexpress,dc=com
+objectClass: inetOrgPerson
+objectClass: organizationalPerson
+objectClass: person
+objectClass: top
+cn: Turanga Leela
+sn: Turanga
+description: Mutant
+employeeType: Captain
+employeeType: Pilot
+givenName: Leela",
+    );
+
+    let item = client
+        .search_one(
+            SearchRequest::builder()
+                .base_dn(server.base_dn())
+                .scope(SearchRequestScope::WholeSubtree)
+                .filter("(&(objectClass=inetOrgPerson)(sn=Turanga))")
+                .build()
+                .unwrap(),
+        )
+        .await
+        .unwrap()
+        .unwrap();
+
+    assert_eq!(
+        item.dn,
+        "cn=Turanga Leela,ou=people,dc=planetexpress,dc=com"
+    );
+
+    client.unbind().await.unwrap();
 }
